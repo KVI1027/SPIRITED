6525321e9edbec3fb253046ed2a131ff2457e305
KVI1027 <103395458+KVI1027@users.noreply.github.com>
2023-02-23 11:13:49 +0800

    整理 code style 清除無用代碼

--- JS/other.js
+++ JS/other.js
@@ -4,16 +4,10 @@
 window.addEventListener('beforeunload', function () {
     window.scrollTo(0, 0);
 });
-// window.addEventListener('load', function () {
-//     document.querySelector('#loading').style.display = 'none';
-// });
 
 const menu_btn = document.querySelector(".menu-btn");
 const nav = document.querySelector("nav");
 const a = document.querySelectorAll(".list-a");
-const news = document.querySelector('.news');
-const news_phone = document.querySelector('.news-phone');
-
 const navHamburger = document.querySelectorAll('.menu-btn-line')
 const banner = document.querySelector('.banner')
 const banner_title = document.querySelector('.banner-title')
@@ -44,39 +38,47 @@ const Footer = document.querySelector("footer")
 const h = window.innerHeight;
 const y = document.documentElement.scrollHeight - h;
 
+// Footer
 function updateFooter() {
+
     //原本數值未考慮視窗高度改變後對文檔高度產生的影響，因此再不改動原先數據下使其自適應螢幕寬度改變數值，以達等比例觸發變動的效果。
     const scroll = (window.pageYOffset * 8714 / y);
-    if (document.body.clientWidth < 800) { 
+
+    if (document.body.clientWidth < 800) {
         const scroll = (window.pageYOffset * 5300 / y);
         footermobile(scroll);
     } else {
-       
         footerpc(scroll);
     }
-    function footerpc(scroll) {
 
+    function footerpc(scroll) {
 
         if (scroll > 8500) {
             Footer.style.bottom = `0px`
         } else if (scroll <= 8500) {
             Footer.style.bottom = `-100%`
         }
+
     }
+
     function footermobile(scroll) {
+
         if (scroll > 5200) {
             Footer.style.bottom = `0px`
         } else if (scroll <= 5200) {
             Footer.style.bottom = `-100%`
             Footer.style.transition = '1s ease'
         }
+
     }
 
-
 }
+
 // nav變色
 function updateNav() {
+
     const scroll = (window.pageYOffset * 8714 / y);
+
     if (scroll < 800) {
         navHamburger.forEach(e => {
             e.classList.remove('menu-btn-line-dark');
@@ -93,6 +95,7 @@ function updateNav() {
 
 // banner區塊
 function updateBanner() {
+
     const scroll = (window.pageYOffset * 8714 / y);
     if (scroll < 200) {
         // 初始
@@ -100,30 +103,12 @@ function updateBanner() {
     } else if (scroll >= 200 && scroll < 1600) {
         banner.style.filter = `brightness(${1 - (scroll - 200) / 1600})`
     }
+
     banner_title.style.left = `-${2.5 * scroll}px`
+
 }
-// function updateShopFrame() {
-//     const scroll = (window.pageYOffset * 8714 / y);
-//     if (scroll < 1600) {
-//         shop_frame.style.width = '91%'
-//         shop_frame.style.margin = `0 0 0 -45.5%`
-//         shop_frame.style.top = `${1000 - (scroll) / 2}px`
-//         shop_frame.style.borderRadius = ` ${1600 - (scroll - 300)}px`
-//     } else if (scroll >= 1600 && scroll < 2600) {
-//         shop_frame.style.width = `100%`
-//         shop_frame.style.margin = `0 0 0 -50%`
-//         shop_frame.style.top = `${1000 - (scroll - 750) / 1}px`
-//         shop_frame.style.borderRadius = ` ${1600 - (scroll - 300)}px`
-//     } else if (scroll >= 3000 && scroll < 5000) {
-//         shop_frame.style.filter = `brightness(${1 - (scroll - 3000) / 3000})`
-//     }
-//     if (scroll >= 4000) {
-//         shop_frame.style.top = `${250 - (scroll) / 4}px`
-//     }
 
-// }
-
-// MENU區塊 動畫
+// Menu
 function updateShopFrame() {
 
     const scroll = (window.pageYOffset * 8714 / y);
@@ -138,8 +123,10 @@ function updateShopFrame() {
 
     // 小尺寸
     function updateStylesShopFrameSmallScreens(scroll) {
+
         let top, width, borderRadius, margin, display;
         const vh = window.innerHeight;
+
         if (scroll < 1600) {
             display = 'block'
             top = 900 - (scroll) / 1;
@@ -156,8 +143,7 @@ function updateShopFrame() {
             width = 100;
             borderRadius = 500 - (scroll) / 2;
             margin = '0 0 0 -50%';
-        }
-        else if (scroll >= 1600 && scroll < 2400) {
+        } else if (scroll >= 1600 && scroll < 2400) {
             top = -1750 + 1.27 * vh - (scroll) / 4;
             display = 'block'
         } else if (scroll >= 2400) {
@@ -166,11 +152,13 @@ function updateShopFrame() {
 
         // 計算 filter 的值
         let brightness;
+
         if (scroll >= 1600 && scroll < 5000) {
             brightness = `brightness(${1 - (scroll - 1600) / 1600})`;
         } else {
             brightness = 'brightness(1)'
         }
+
         shop_frame.style.top = `${top}px`;
         shop_frame.style.width = `${width}%`;
         shop_frame.style.borderRadius = `${borderRadius}px`;
@@ -182,6 +170,7 @@ function updateShopFrame() {
 
     // 大尺寸
     function updateStylesShopFrameLargeScreens(scroll) {
+
         let top, width, borderRadius, margin, display;
 
         if (scroll < 1800) {
@@ -193,6 +182,7 @@ function updateShopFrame() {
         } else {
             display = 'none'
         }
+
         if (scroll < 500) {
             width = 93;
             margin = '0 0 0 -46.5%';
@@ -203,12 +193,12 @@ function updateShopFrame() {
 
         // 計算 filter 的值
         let brightness;
+
         if (scroll >= 3200 && scroll < 5000) {
             brightness = `brightness(${1 - (scroll - 3200) / 2000})`;
         } else {
             brightness = 'brightness(1)'
         }
-
 
         shop_frame.style.top = `${top}px`;
         shop_frame.style.width = `${width}%`;
@@ -216,70 +206,16 @@ function updateShopFrame() {
         shop_frame.style.margin = `${margin}`;
         shop_frame.style.filter = `${brightness}`;
         shop_frame.style.display = `${display}`;
+
     }
-    // 計算 top 和 borderRadius 的值
-    // let top, width, borderRadius, margin, display;
 
-    // if (screenwidth < 800 && scroll < 1600) {
-    //     display = 'block'
-    //     top = 800 - (scroll) / 1;
-    //     width = 100;
-    //     borderRadius = 400 - (scroll) /2;
-    //     margin = '0 0 0 -50%';
-    // } else if (scroll > 200) {
-    //     top = 1200 - (scroll);
-    //     borderRadius = 2000 - 1.5*(scroll);
-    // }
-    // if (scroll < 500) {
-    //     width = 93;
-    //     margin = '0 0 0 -46.5%';
-    // } else if (scroll >= 500 && scroll < 1200) {
-    //     width = 100;
-    //     margin = '0 0 0 -50%';
-    // }else if (screenwidth < 800 && scroll < 700) {
-    //     top = 800 - (scroll) / 1;
-    //     width = 91;
-    //     borderRadius = 400 - (scroll) / 2 ;
-    //     margin = '0 0 0 -45.5%';
-    // } else if (screenwidth < 800 && scroll >= 700 && scroll < 1600) {
-    //     top = 800 - (scroll) / 1;
-    //     width = 100;
-    //     borderRadius = 400 - (scroll) /2;
-    //     margin = '0 0 0 -50%';
-    // } 
-    // else if (screenwidth < 800 && scroll >= 1600 && scroll < 2400) {
-    //     top = 500 - (scroll + 4800) / 4;
-    //     display = 'block'
-    // } else if (screenwidth < 800 && scroll >= 2400) {
-    //     display = 'none'
-    // } else if (scroll >= 4000) {
-    //     top = 250 - (scroll) / 4;
-    // }
-
-    // // 計算 filter 的值
-    // let brightness;
-    // if (screenwidth < 800 && scroll >= 1600 && scroll < 5000) {
-    //     brightness = `brightness(${1 - (scroll - 1600) / 1600})`;
-    // }else if (screenwidth < 800) {
-    //     brightness = 'brightness(1)'
-    // }else if (scroll >= 3200 && scroll < 5000) {
-    //     brightness = `brightness(${1 - (scroll - 3200) / 2000})`;
-    // }else{
-    //     brightness = 'brightness(1)'
-    // }
-
-
-    // shop_frame.style.top = `${top}px`;
-    // shop_frame.style.width = `${width}%`;
-    // shop_frame.style.borderRadius = `${borderRadius}px`;
-    // shop_frame.style.margin = `${margin}`;
-    // shop_frame.style.filter = `${brightness}`;
-    // shop_frame.style.display = `${display}`;
 }
 
 // 如何購買區塊
 function updateHowToBuy() {
+
     const scroll = (window.pageYOffset * 8714 / y);
+
     if (document.body.clientWidth < 800) {
         const scroll = (window.pageYOffset * 5300 / y);
         updateStylesHowToBuySmallScreens(scroll);
@@ -287,11 +223,15 @@ function updateHowToBuy() {
         updateStylesHowToBuyLargeScreens(scroll);
     }
 
+    // 大尺寸
     function updateStylesHowToBuyLargeScreens(scroll) {
+
         let how_to_buy_top, how_to_buy_borderRadius, how_left_width, how_right_width, how_right_top, how_dscp_height, how_midline_height;
+
         if (scroll >= 2500) {
             how_to_buy_borderRadius = 1600 - (scroll - 2500)
         }
+
         if (scroll >= 2500 && scroll <= 3800) {
             how_to_buy_top = 1000 - (scroll - 2500) / 2
         } else if (scroll > 3800) {
@@ -299,6 +239,7 @@ function updateHowToBuy() {
         } else {
             how_to_buy_top = 2000
         }
+
         if (scroll >= 4000 && scroll <= 4800) {
             how_left_width = 65 - (scroll - 4000) / 30
             how_right_width = 25 + (scroll - 4000) / 30
@@ -317,6 +258,7 @@ function updateHowToBuy() {
             how_right_top = 0
             how_to_buy_top = 2500
         }
+
         how_dscp_li.forEach(e => {
 
             if (scroll <= 4800) {
@@ -339,8 +281,8 @@ function updateHowToBuy() {
                 e.children[1].innerHTML = ""
                 e.children[1].style.height = '0'
             }
-        });
 
+        });
 
         how_to_buy.style.top = `${how_to_buy_top}px`
         how_to_buy.style.borderRadius = `${how_to_buy_borderRadius}px`
@@ -349,13 +291,18 @@ function updateHowToBuy() {
         how_right.style.top = `${how_right_top}px`
         how_dscp.style.height = `${how_dscp_height}`
         how_midline.style.height = `${how_midline_height}%`
+
     }
+
+    // 小尺寸
     function updateStylesHowToBuySmallScreens(scroll) {
+
         let how_to_buy_top, how_to_buy_borderRadius, how_left_width, how_right_width, how_right_top, how_dscp_height, how_midline_height;
 
         if (scroll >= 1500) {
             how_to_buy_borderRadius = 500 - (scroll - 1500) / 2
         }
+
         if (scroll < 1500) {
             how_to_buy_top = 750 - (scroll - 1500) / 1
         } else if (scroll >= 1500 && scroll <= 2800) {
@@ -363,6 +310,7 @@ function updateHowToBuy() {
         } else if (scroll > 2800) {
             how_to_buy_top = 750 - (scroll - 1500) / 1
         }
+
         if (scroll <= 2700) {
             how_left_width = 90
             how_right_width = 90
@@ -372,6 +320,7 @@ function updateHowToBuy() {
         } else {
             how_dscp_height = `${272 - 272 * (scroll - 3400) / 250}px`
         }
+
         how_dscp_li.forEach(e => {
 
             if (scroll <= 2700) {
@@ -390,6 +339,7 @@ function updateHowToBuy() {
                 e.children[1].innerHTML = ""
                 e.children[1].style.height = '0'
             }
+
         });
 
         how_to_buy.style.top = `${how_to_buy_top}px`
@@ -399,27 +349,21 @@ function updateHowToBuy() {
         how_right.style.top = `${how_right_top}px`
         how_dscp.style.height = `${how_dscp_height}`
         how_midline.style.height = `${how_midline_height}%`
+
     }
-    //RWD
-    // if (document.body.clientWidth < 1366) {
-    //     how_dscp.style.height = `${20 + (scroll - 4600) / 10}vh`
-    //     if (scroll >= 4600 && scroll <= 5400) {
-    //         how_left.style.width = `${55 - (scroll - 4600) / 30}%`
-    //         how_right.style.width = `${35 + (scroll - 4600) / 30}%`
-    //     } else if (scroll > 5400 && scroll < 5800) {
-    //         how_left.style.width = `28.3%`
-    //         how_right.style.width = `61.6%`
-    //     }
-    // }
+    
 }
 
 // 關於我們區塊
 function updateAboutUs() {
+
     const scroll = (window.pageYOffset * 8714 / y);
 
-
+    // 尺寸適應
     if (document.body.clientWidth < 800) {
+
         const scroll = (window.pageYOffset * 5300 / y);
+
         if (scroll < 3500) {
             aboutus.style.top = `${800 - (scroll - 3300) / 1}px`
             aboutus_BG.style.display = 'none'
@@ -431,9 +375,11 @@ function updateAboutUs() {
         } else if (scroll > 7400) {
             aboutus.style.top = `-2900px`
         }
+
         if (scroll >= 3600 && scroll <= 4100) {
             aboutus_h1.style.opacity = '1'
         }
+
         if (scroll >= 4100 && scroll <= 4700) {
             //Aboutus變色效果
             const [red, green, blue] = [241, 233, 225]
@@ -501,17 +447,19 @@ function updateAboutUs() {
                 e.style.transition = `0.3s ease`
             });
         }
+
         if (scroll <= 6700) {
             aboutus_BG.style.top = `0px`
-        }
-        else if (scroll > 6700 && scroll < 7400) {
+        } else if (scroll > 6700 && scroll < 7400) {
             aboutus_BG.style.top = `${0 - (scroll - 6700) / 1}px`
         } else {
             aboutus_BG.style.top = `-700px`
         }
 
     } else {
+
         const scroll = (window.pageYOffset * 8714 / y);
+
         if (scroll < 5600) {
             aboutus.style.top = `${1000 - (scroll - 5600) / 1}px`
         } else if (scroll >= 5600 & scroll <= 8300) {
@@ -520,14 +468,11 @@ function updateAboutUs() {
         } else {
             aboutus.style.top = `${-2400 - (scroll - 8300) / 1}px`
         }
-        // else if (scroll <= 9000) {
-        //     aboutus.style.top = `${1000 - (scroll - 5600) / 1}px`
-        // } else if (scroll > 9000) {
-        //     aboutus.style.top = `-2900px`
-        // }
+
         if (scroll >= 6600 && scroll <= 7100) {
             aboutus_h1.style.opacity = '1'
         }
+
         if (scroll >= 7100 && scroll <= 7700) {
             //Aboutus變色效果
             const [red, green, blue] = [241, 233, 225]
@@ -594,55 +539,50 @@ function updateAboutUs() {
                 e.style.transition = `0.3s ease`
             });
         }
+
         if (scroll <= 8300) {
             aboutus_BG.style.top = `0px`
         }
-        // else if (scroll > 8300 && scroll < 9000) {
-        //     aboutus_BG.style.top = `${0 - (scroll - 8300) / 1}px`
-        // } else {
-        //     aboutus_BG.style.top = `-700px`
-        // }
+
     }
+
 }
 
-
+// 呼叫函式
 window.addEventListener('scroll', function () {
+
     const scroll = (window.pageYOffset * 8714 / y);
-    console.log(window.pageYOffset);
-    console.log(window.innerHeight);
     window.requestAnimationFrame(updateNav);
     window.requestAnimationFrame(updateBanner);
     window.requestAnimationFrame(updateShopFrame);
     window.requestAnimationFrame(updateHowToBuy);
     window.requestAnimationFrame(updateAboutUs);
     window.requestAnimationFrame(updateFooter);
+
 });
-
-
 
 //印製 如何購買文字內容
 var how_detail = [{
     detail: `Our products are limited.
-    Booking desserts on the day ,please call us directly: (04) 2378 - 0238. Otherwise
-    please leave a message through FB or IG. We closed on Tuesday and Wednesday.<br/>
-    現場購買，手工製做，數量有限
-當天預留：請直接打電話到店裡(04) 2378 - 0238
+Booking desserts on the day ,please call us directly: (04) 2378 - 0238. Otherwise
+please leave a message through FB or IG. We closed on Tuesday and Wednesday.<br/>
+現場購買，手工製做，數量有限<br/>
+當天預留：請直接打電話到店裡(04) 2378 - 0238<br/>
 提前預留：請在禮拜二、三公休日之外，私訊 FB或IG`
 }, {
     detail: `Please ask us through FB or IG message.<br/>
-    請私訊FB或IG詢問`
+請私訊FB或IG詢問`
 }, {
     detail: `Please order at least a week previously through IG or FB message.
-    Huge order need at least 2-3 weeks.
-    Spirited Bakery is available on UberEats as well.<br/>
-    請提前⾄少1-2個禮拜，⼤量請提前2-3個禮拜，私訊FB或IG
-在UberEats App上搜尋「Spirited Bakery」，有限定可外送範圍`
+Huge order need at least 2-3 weeks.
+Spirited Bakery is available on UberEats as well.<br/>
+請提前⾄少1-2個禮拜，⼤量請提前2-3個禮拜，私訊FB或IG。在UberEats App上搜尋「Spirited Bakery」，有限定可外送範圍`
 }]
 var str = "";
 
-// 如何購買 展開動畫
+// 如何購買 展開內容
 how_dscp_li.forEach((e, index) => {
-    console.log(e.children[0].children[0].children[0]);
+
     e.addEventListener('click', function () {
 
         if (e.dataset.state == 'off') {
@@ -661,27 +601,32 @@ how_dscp_li.forEach((e, index) => {
         }
 
     }, false);
-
+    
 });
-
 
 // 選單列開關JS
 menu_btn.onclick = function () {
+
     if (document.querySelector("#menu-btn").checked) {
+
         closeList();
+
         let shop_distance = (document.querySelector(".shop").offsetTop - 100)
         let shop_height = document.querySelector(".shop").offsetHeight
+
         if ((document.documentElement.scrollTop >= shop_distance) && (document.documentElement.scrollTop < (shop_distance + shop_height))) {
             document.querySelectorAll('.menu-btn-line').forEach(element => {
                 element.classList.remove('menu-btn-line-light');
                 element.classList.add('menu-btn-line-dark');
             });
         }
+
     }
     else {
 
         nav.classList.add('d-flex');
         nav.classList.remove('d-none');
+
         setTimeout(function () {
             nav.style = "opacity:1;";
         }, 100);
@@ -690,15 +635,21 @@ menu_btn.onclick = function () {
             element.classList.add('menu-btn-line-light');
             element.classList.remove('menu-btn-line-dark');
         });
+
     }
 }
+
 //漢堡條樣式切換JS
 function clicklinkEvent(index) {
+
     closeList();
     document.querySelector('input').checked = false;
     clickEvent(index);
+
 }
+
 function closeList() {
+
     nav.style = "opacity:0;";
     setTimeout(function () {
         nav.classList.remove('d-flex');
@@ -711,20 +662,24 @@ function closeList() {
 a.forEach((element, index) => {
 
     element.onclick = function () {
+
         closeList();
         document.querySelector('input').checked = false;
         if (index == 0) {
             to_how_to_buy();
-        }
-        else {
+        } else {
             to_about_us();
         }
     }
+
 });
 
 //跳轉
 function clicklinkEvent(index) {
+
+    //大小尺寸轉跳 
     if (document.body.clientWidth < 800) {
+
         if (index === 0) {
             closeList();
             document.querySelector('input').checked = false;
@@ -738,7 +693,9 @@ function clicklinkEvent(index) {
             document.querySelector('input').checked = false;
             window.scrollTo(0, 3300);
         }
+
     } else {
+
         if (index === 0) {
             closeList();
             document.querySelector('input').checked = false;
@@ -752,66 +709,11 @@ function clicklinkEvent(index) {
             document.querySelector('input').checked = false;
             window.scrollTo(0, 5400);
         }
+
     }
+
 }
 
-
-//看全部菜單按按鈕打開、點背景關閉
-function view_all_menu() {
-    let view_menu;
-    if (document.body.clientWidth > 1199) {
-        view_menu = document.querySelector('.view-menu');
-
-    }
-    else {
-        view_menu = document.querySelector('.view-menu-phone');
-    }
-    view_menu.style = "opacity:1";
-    view_menu.classList.remove('d-none');
-}
-function view_all_menu_close() {
-    let view_menu
-    if (document.body.clientWidth > 1199) {
-        view_menu = document.querySelector('.view-menu');
-    }
-    else {
-        view_menu = document.querySelector('.view-menu-phone');
-    }
-
-    if (view_menu == event.target) {
-        view_menu.style = "opacity:0";
-        setTimeout(function () {
-            view_menu.classList.add('d-none');
-        }, 1000);
-    }
-}
-
-//關閉最新消息
-function closeNews() {
-    news.style = 'display:none';
-    news_phone.style = 'display:none';
-    window.scrollTo(0, 0)
-
-}
-// news.addEventListener('click', event => {
-//     if (news == event.target) {
-//         news.style = "opacity:0";
-//         setTimeout(function () {
-//             news.style = 'display:none'
-//         }, 1000);
-//         window.scrollTo(0, 0)
-//     }
-// });
-
-news_phone.addEventListener('click', event => {
-    if (news_phone == event.target) {
-        news_phone.style = "opacity:0";
-        setTimeout(function () {
-            news_phone.style = 'display:none'
-        }, 1000);
-        window.scrollTo(0, 0)
-    }
-});
-
-
-
+// 測試
+console.log(window.pageYOffset);
+console.log(window.innerHeight);
